9e5cf18319f71453259768a5c8e835aada19c227
Michael Landis <michael4css@gmail.com>
2019-10-07 21:39:01 -0700

    Added landing tests

---
+++ app/src/components/Landing/Landing.test.js
@@ -0,0 +1,244 @@
+import React from 'react';
+import { MemoryRouter, Route, Redirect } from 'react-router-dom';
+import { shallow, mount, render } from 'enzyme';
+
+import { useDispatch, setState, getDispatchArguments, getUseSelectorArguments, clearArguments } from 'react-redux';
+
+import { fetchConfig } from '../../reducers/config';
+import { loadStories } from '../../reducers/stories';
+import { loadWords } from '../../reducers/words';
+
+import Landing from './';
+
+import Modal from '../../layouts/Modal';
+import Copyright from '../../elements/Copyright';
+import Image from '../../elements/Image';
+import ProgressIndicator from '../../elements/ProgressIndicator';
+import Title from '../../elements/Title';
+
+const initialState = {
+	config: {
+		loading: false,
+		loaded: false,
+		wordSources: [],
+		storySource: {
+			loaded: false
+		}
+	}
+};
+
+/*
+ *	Note: I place empty line comments after JSX because my editor can't distinguish between closing JSX tags and starting regexes. ¯\_(ツ)_/¯
+ */
+describe('<Landing/>', () => {
+
+	setState(initialState);
+
+	afterEach(() => {
+		clearArguments();
+	});
+	/*
+	 *	The tests are done in order, with progressive completion of initialState until the last test where the buildout is complete.
+	 */
+
+	test('Initial opening', () => {
+		const output = render(
+			<MemoryRouter initialEntries={['/']}>
+				<Landing/>
+			</MemoryRouter>
+		); //
+
+		const expectedOutput = render(
+			<Modal open={true} fade={false} background="#FFF" close={false}>
+				<Image src="/development/madlibs/logo.png" align="center"/>
+				<Title>MadLibs, React style</Title>
+				<ProgressIndicator current={0} max={1} width="80%" backgroundColor="#DEF" />
+				<Copyright/>
+			</Modal>
+		);//
+
+		expect(output).toEqual(expectedOutput);
+
+		const [thunk] = getDispatchArguments();
+		expect(typeof thunk).toBe("function");
+	});
+
+	test('Config load presentation', () => {
+		initialState.config = {
+			loading: true,
+			loaded: false,
+			wordSources: [
+				{
+					url: 'b',
+					loaded: false
+				},
+				{
+					url: 'c',
+					loaded: false
+				}
+			],
+			storySource: {
+				url: 'a',
+				loaded: false
+			}
+		};
+
+		const output = render(
+			<MemoryRouter initialEntries={['/']}>
+				<Landing/>
+			</MemoryRouter>
+		); //
+
+		const expectedOutput = render(
+			<Modal open={true} fade={false} background="#FFF" close={false}>
+				<Image src="/development/madlibs/logo.png" align="center"/>
+				<Title>MadLibs, React style</Title>
+				<ProgressIndicator current={0} max={3} width="80%" backgroundColor="#DEF" />
+				<Copyright/>
+			</Modal>
+		);//
+
+		expect(output).toEqual(expectedOutput);
+	});
+
+	test('Word load presentation', () => {
+		initialState.config.wordSources[0] = {
+			id: 'b',
+			title: 'B',
+			help: 'B help',
+			words: ['b1', 'b2'],
+			loaded: true
+		};
+
+		const output = render(
+			<MemoryRouter initialEntries={['/']}>
+				<Landing/>
+			</MemoryRouter>
+		); //
+
+		const expectedOutput = render(
+			<Modal open={true} fade={false} background="#FFF" close={false}>
+				<Image src="/development/madlibs/logo.png" align="center"/>
+				<Title>MadLibs, React style</Title>
+				<ProgressIndicator current={1} max={3} width="80%" backgroundColor="#DEF" />
+				<Copyright/>
+			</Modal>
+		);//
+
+		expect(output).toEqual(expectedOutput);
+	});
+
+	test('Story load presentation', () => {
+		initialState.config.storySource = {
+			stories: [
+				{
+					id: 'x',
+					title: 'X',
+					fields: ['a', 'b'],
+					template: 'Story X'
+				},
+				{
+					id: 'y',
+					title: 'Y',
+					fields: ['b', 'a'],
+					template: 'Story Y'
+				}
+			],
+			loaded: true
+		};
+
+		const output = render(
+			<MemoryRouter initialEntries={['/']}>
+				<Landing/>
+			</MemoryRouter>
+		); //
+
+		const expectedOutput = render(
+			<Modal open={true} fade={false} background="#FFF" close={false}>
+				<Image src="/development/madlibs/logo.png" align="center"/>
+				<Title>MadLibs, React style</Title>
+				<ProgressIndicator current={2} max={3} width="80%" backgroundColor="#DEF" />
+				<Copyright/>
+			</Modal>
+		);//
+
+		expect(output).toEqual(expectedOutput);
+	});
+
+	test('Final word load presentation', () => {
+		initialState.config.wordSources[1] = {
+			id: 'a',
+			title: 'A',
+			ref: 'b',
+			loaded: true
+		};
+
+		const output = render(
+			<MemoryRouter initialEntries={['/']}>
+				<Landing/>
+			</MemoryRouter>
+		); //
+
+		const expectedOutput = render(
+			<Modal open={true} fade={false} background="#FFF" close={false}>
+				<Image src="/development/madlibs/logo.png" align="center"/>
+				<Title>MadLibs, React style</Title>
+				<ProgressIndicator current={3} max={3} width="80%" backgroundColor="#DEF" />
+				<Copyright/>
+			</Modal>
+		);//
+
+		expect(output).toEqual(expectedOutput);
+	});
+
+	test('Load complete presentation', () => {
+		initialState.config.loaded = true;
+
+		const output = render(
+			<MemoryRouter initialEntries={['/']}>
+				<Landing/>
+			</MemoryRouter>
+		); //
+
+		const expectedOutput = render(
+			<Modal open={false} fade={true} background="#FFF" close={false}>
+				<Image src="/development/madlibs/logo.png" align="center"/>
+				<Title>MadLibs, React style</Title>
+				<ProgressIndicator current={3} max={3} width="80%" backgroundColor="#DEF" />
+				<Copyright/>
+			</Modal>
+		);//
+
+		expect(output).toEqual(expectedOutput);
+
+		const actions = getDispatchArguments();
+		expect(typeof actions.pop()).toBe("function");
+
+		const sorter = ({type: a}, {type: b}) => a < b ? -1 : (a > b ? 1 : 0);
+
+		const expectedActions = [
+			loadStories(initialState.config.storySource.stories),
+			loadWords(initialState.config.wordSources)
+		].sort(sorter);
+
+		expect(actions.sort(sorter)).toEqual(expectedActions);
+	});
+
+	test('Application start', () => {
+		initialState.config.loading = false;
+
+		const output = render(
+			<MemoryRouter initialEntries={['/']}>
+				<Landing/>
+			</MemoryRouter>
+		); //
+
+		const expectedOutput = render(
+			<MemoryRouter initialEntries={['/']}>
+				<Redirect to="/stories"/>
+			</MemoryRouter>
+		);//
+
+		expect(output).toEqual(expectedOutput);
+	});
+});
--- app/src/components/__mocks__/react-redux.js
+++ app/src/components/__mocks__/react-redux.js
@@ -15,7 +15,25 @@ const useSelector = jest.fn((selector, equalityFn) => {
 	return selector(state);
 });
 
-const getDispatchArguments = () => [...dispatchArguments];
+// duplicated from common.js to avoid dependencies
+const __sleep = async (time) => (new Promise(resolve => setTimeout(resolve, ms)));
+
+const getDispatchArguments = (expectedCount = 0, maxTime = 5000) => {
+	if (expectedCount === 0) {
+		return [...dispatchArguments];
+	}
+	else {
+		return async () => {
+			const start = Date.now();
+			let end = start;
+			while ( (end - start < maxTime) && (dispatchArguments.length < expectedCount) ) {
+				await __sleep(100);
+				end = Date.now();
+			}
+			return dispatchArguments.length == expectedCount ? [...dispatchArguments] : null;
+		};
+	}
+};
 
 const getUseSelectorArguments = () => selectorArguments.map(selector => ({...selector}));
 
